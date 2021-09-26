7ab087aef3efadaf996182b6747c458ffbf9f81f
adel-ks <adelmanaeva@gmail.com>
2021-09-26 22:03:28 +0200

    Created views using Serializers

--- news/views.py
+++ news/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import generics
+from .models import *
+from .serializers import NewsListSerializer, NewsDetailSerializer
 
-# Create your views here.
+
+class NewsListView(generics.ListAPIView):
+	queryset = News.objects.all()
+	serializer_class = NewsListSerializer
+
+
+class NewsDetailView(generics.RetrieveAPIView):
+	queryset = News.objects.all()
+	serializer_class = NewsDetailSerializer
