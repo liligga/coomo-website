7081f03fa8df8ed26ea70e72b7f114a83b454739
HikkariWest <gosha.troncraft@gmail.com>
2021-09-28 00:55:34 +0600

    Добавил два теста для курсов и осталось добавить еще один для курсов WIP

--- videocourses/tests.py
+++ videocourses/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status
+from .models import *
+from .serializers import CourseDetailSerializer
 
-# Create your tests here.
+
+class TestCourse(APITestCase):
+	def setUp(self):
+		self.test_course = Course.objects.create(
+			name='Test Course', 
+			description='Test description', 
+			lang_course='Ru')
+		self.test_video = Video.objects.create(course=self.test_course, 
+			name='test_name', 
+			video_link='https://www.test_link.ru', 
+			lang_video='Ru')
+
+	def test_course_list(self):
+		response = self.client.get(reverse('course_list'))
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+		self.assertEqual(len(response.data), 1)
+		self.assertTrue({'id':1, 'name':'Test Course','description':'Test description', 'lang_course':'Ru'}) in response.json()
+
+	def test_course_detail(self):
+		response = self.client.get(reverse('course_detail', kwargs={'pk':self.test_course.id}))
+		serializer_data = CourseDetailSerializer(self.test_course).data
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+		self.assertEqual(serializer_data, response.data)
+
