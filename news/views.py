4d99c64149815bb43e896b302afb89f9245a9b6f
adel-ks <adelmanaeva@gmail.com>
2021-10-11 22:34:54 +0200

    New view for model

--- news/views.py
+++ news/views.py
@@ -1,28 +1,31 @@
-from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework import generics
+from rest_framework.views import APIView
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import *
-from .serializers import NewsListSerializer, NewsDetailSerializer
+from .serializers import NewsLastestSerializer, NewsImportantSerializer, NewsBannerSerializer
 
 
-class NewsListView(generics.ListAPIView):
-	queryset = News.objects.all()#[:8]
-	serializer_class = NewsListSerializer
-	filter_backends = [DjangoFilterBackend] 
-	filterset_fields = ['important', 'language']
+class NewsView(APIView):	
+	def get(self, request):
+		last_eight_news = News.objects.order_by('-created')[:8]
+		important_news = News.objects.filter(important=True)
+		banners = News.objects.all()
+		serializer1 = NewsLastestSerializer(last_eight_news, many=True)
+		serializer2 = NewsImportantSerializer(important_news, many=True)
+		serializer3 = NewsBannerSerializer(banners, many=True)
+		context = {'last_eight_news':serializer1.data,
+					'important_news':serializer2.data,
+					'banners':serializer3.data}
 
-	
-
-
-# def last_four_news(self, request, ):
-# 	news = News.objects.order_by('-created')[:4]
-# 	return news
+		return Response(context)
 
 
 
 
-# class NewsDetailView(generics.RetrieveAPIView):
-# 	queryset = News.objects.all()
-# 	serializer_class = NewsDetailSerializer
+class NewsDetailView(APIView):
+	def get(self, request):
+		news = News.objects.all().latest('id')[:4]
+		serializer = NewsSerializer(news)
+		return Response (serializer.data)
 
+			
