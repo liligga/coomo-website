36e8f4dcb6d851a851d93482c171da6786b30ee8
Igor <8luxfshdnlz1@mail.ru>
2021-10-21 15:20:27 +0600

    Removed requirements, fixed test views and seri

--- online_test/serializers.py
+++ online_test/serializers.py
@@ -2,7 +2,15 @@ from rest_framework import serializers
 from .models import *
 
 
+class OnlineTestQuestionSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = OnlineTestQuestion
+		fields = ('question',)
+
+
 class OnlineTestListSerializer(serializers.ModelSerializer):
+	questions = OnlineTestQuestionSerializer(many = True)
+
 	class Meta:
 		model = OnlineTest
-		fields = ('name', 'part_num', 'version', 'duration', 'num_questions', 'language')
+		fields = ('name', 'intro', 'part_num', 'version', 'duration', 'num_questions', 'language', 'questions')
--- online_test/views.py
+++ online_test/views.py
@@ -1,10 +1,10 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import *
-from rest_framework import generics
+from rest_framework import viewsets, generics
 from .serializers import OnlineTestListSerializer
 
 
-class OnlineTestListView(generics.ListAPIView):
+class OnlineTestViewSet(viewsets.ReadOnlyModelViewSet):
 	queryset = OnlineTest.objects.all()
 	serializer_class = OnlineTestListSerializer
 	filter_backends = [DjangoFilterBackend]
