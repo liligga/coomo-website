a53bdd0a60128d816e5fd6f3dc93dc3047bdccbd
HikkariWest <gosha.troncraft@gmail.com>
2021-10-26 20:52:04 +0600

    Добавил закомментированный сигнал изменения размера картинок

--- news/models.py
+++ news/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.dispatch import receiver
 from django.contrib.auth.models import User
 from django.utils.text import slugify
 from ckeditor_uploader.fields import RichTextUploadingField
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
+import imghdr
 from PIL import Image
 from pytils.translit import slugify
 from django.template.defaultfilters import truncatewords
@@ -23,12 +25,12 @@ class News(models.Model):
 	slug = models.SlugField(blank=True, null=True, unique = True)
 	article = RichTextUploadingField(verbose_name='Статья')
 	excerpt = models.TextField(blank=True, null=True, verbose_name='Отрывок из статьи')
-	important = models.BooleanField(default = False, verbose_name = 'Важное')
+	important = models.BooleanField(default = False, verbose_name='Важное')
 	language = models.CharField(max_length=15, choices=LANGUAGE, verbose_name='Язык')
 	author = models.ForeignKey(User, default=True, on_delete=models.CASCADE, verbose_name='Автор')
 	created = models.DateTimeField(auto_now_add=True, verbose_name='Создано')
 	updated = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
-	cover = models.ImageField(upload_to='uploads', blank=True)
+	cover = models.ImageField(upload_to='news', verbose_name='Обложка новости')
 	banners = models.BooleanField(default=False, verbose_name='Баннер')
 
 	def __str__(self):
@@ -45,6 +47,18 @@ class News(models.Model):
 		verbose_name = 'Новость'
 		verbose_name_plural = 'Новости'
 
+# @receiver(post_save, sender=News)
+# def image_post_save(sender, instance, *args, **kwargs):
+# 	if instance.banners:
+# 		preview_cover = Image.open(instance.cover)
+# 		extension = imghdr.what(instance.cover)
+# 		cover_size = (200, 350)
+# 		preview_cover.thumbnail(cover_size)
+# 		preview_cover.save(f'{instance.cover}-resized'+extension)
+# 	# if created:
+# 	# 	new_news = News.objects.get_or_create(news=instance)
+
+# post_save.connect(image_post_save, sender=News)
 
 def news_pre_save(sender, instance, *args, **kwargs):
 	if instance.important:
