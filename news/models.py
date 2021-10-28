099bdf22f2829dcd17cef3d839a9ed85406f07ba
Дмитрий Макаров <maggot8950@gmail.com>
2021-10-28 09:27:15 +0600

    Очистка html при сохранении excerpt в модели News

--- news/models.py
+++ news/models.py
@@ -1,3 +1,4 @@
+import re
 from django.db import models
 from django.contrib.auth.models import User
 # from django.utils.text import slugify
@@ -7,6 +8,13 @@ from django.dispatch import receiver
 from PIL import Image
 from pytils.translit import slugify
 from django.template.defaultfilters import truncatewords
+
+CLEAN_RE = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
+
+
+def clean_html(raw_html):
+  clean_text = re.sub(CLEAN_RE, '', raw_html)
+  return clean_text
 
 
 class News(models.Model):
@@ -28,14 +36,15 @@ class News(models.Model):
     cover = models.ImageField(upload_to='uploads', blank=True, verbose_name='Обложка')
     banners = models.BooleanField(default=False, verbose_name='Баннер')
     parent = models.ForeignKey('self', verbose_name='Родительская новость', on_delete=models.SET_NULL, blank=True,
-                               null=True, related_name='news',)
+                               null=True, related_name='news', )
 
     def __str__(self):
         return self.title
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
-        self.excerpt = truncatewords(self.article, 35)
+        clean_text = clean_html(self.article)
+        self.excerpt = truncatewords(clean_text, 35)
         super(News, self).save(*args, **kwargs)
 
     class Meta:
