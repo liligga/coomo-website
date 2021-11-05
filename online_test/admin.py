6fcca8a8c71ff46c9e39b9031d6da728737a74dd
Дмитрий Макаров <maggot8950@gmail.com>
2021-11-05 14:14:10 +0600

    Изменена обработка excel

--- online_test/admin.py
+++ online_test/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+import pandas as pd
+from .forms import ExcelForm
 from .models import *
 
 
@@ -12,6 +14,8 @@ class AnswerInstanceAdmin(admin.TabularInline):
 
 @admin.register(OnlineTest)
 class OnlineTestAdmin(admin.ModelAdmin):
+    excel = ExcelForm
+    readonly_fields = ('excel',)
     list_display = [
         'id',
         'name',
@@ -19,6 +23,24 @@ class OnlineTestAdmin(admin.ModelAdmin):
         'version',
         'language',
         'is_active',
-        'excel_file'
     ]
     inlines = [QuestionInstanceAdmin, AnswerInstanceAdmin]
+    fieldsets = [
+        ('ОНЛАЙН ТЕСТ', {
+            'fields': (
+                'name', 'part_num', 'version', 'duration', 'num_questions', 'num_answers', 'language', 'is_active',
+                'intro', 'excel',)
+        }
+         ),
+    ]
+
+    def save_model(self, request, obj, form, change):
+        file = request.FILES.get('excel_file')
+        obj.save()
+        if file:
+            answers = pd.read_excel(file)
+            for col_n, col_contents in answers.iteritems():
+                if str(col_contents.values[0]).isdigit():
+                    answers = AnswerTest.objects.create(onlinetest=obj, question_number=col_contents.values[0],
+                                                        correct_answer=col_contents.values[1])
+                    answers.save()
--- online_test/models.py
+++ online_test/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from ckeditor.fields import RichTextField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-import pandas as pd
 
 LANGUAGE_CHOICES = [
     ('Ru', 'Русский'),
@@ -26,8 +25,6 @@ class OnlineTest(models.Model):
     language = models.CharField(max_length=15, default='Ru', choices=LANGUAGE_CHOICES, verbose_name='Язык теста')
     is_active = models.BooleanField(default=True, verbose_name='Опубликован')
     intro = RichTextField(verbose_name='Приветственный текст')
-    excel_file = models.FileField(upload_to='ort/excel_answers/', verbose_name='Excel-таблица с ответами', blank=True,
-                                  null=True, help_text='Загрузите таблицу если есть или заполните ответы вручную ниже')
 
     class Meta:
         verbose_name = 'Онлайн тест'
@@ -70,16 +67,3 @@ class AnswerTest(models.Model):
 
     def __str__(self):
         return self.onlinetest.name
-
-
-@receiver(post_save, sender=OnlineTest)
-def get_excel_answers(sender, instance, created, **kwargs):
-    if created:
-        if instance.excel_file:
-            file = instance.excel_file
-            answers = pd.read_excel(file)
-            for col_n, col_contents in answers.iteritems():
-                if str(col_contents.values[0]).isdigit():
-                    answers = AnswerTest.objects.create(onlinetest=instance, question_number=col_contents.values[0],
-                                                        correct_answer=col_contents.values[1])
-                    answers.save()
