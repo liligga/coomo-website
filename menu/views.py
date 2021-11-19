6c8eba51b55e6a72cd62b209ffe98ccb77ffdb39
HikkariWest <gosha.troncraft@gmail.com>
2021-11-20 03:11:54 +0600

    Сделал загрузку меню и футера по умолчанию на русском языке

--- menu/views.py
+++ menu/views.py
@@ -5,14 +5,30 @@ from .models import MenuLink, FooterLink
 
 
 class MenuLinksView(generics.ListAPIView):
-	queryset = MenuLink.active_objects.all()
 	serializer_class = MenuSerializer
 	filter_backends = [DjangoFilterBackend]
 	filterset_fields = ['lang_menu_link']
 
+	def get_queryset(self):
+		query = self.request.query_params.get('lang_menu_link')
+		if query:
+			queryset = MenuLink.active_objects.filter(lang_menu_link=query)
+		else:
+			queryset = MenuLink.active_objects.filter(lang_menu_link='Ru')
+
+		return queryset
+
 
 class FooterLinksView(generics.ListAPIView):
-	queryset = FooterLink.active_objects.all()
 	serializer_class = FooterSerializer
 	filter_backends = [DjangoFilterBackend]
 	filterset_fields = ['lang_footer_link']
+
+	def get_queryset(self):
+		query = self.request.query_params.get('lang_footer_link')
+		if query:
+			queryset = FooterLink.active_objects.filter(lang_footer_link=query)
+		else:
+			queryset = FooterLink.active_objects.filter(lang_footer_link='Ru')\
+
+		return queryset
