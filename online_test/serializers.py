8b003aba029a42d11cd7a85867d720cf1b53702f
Igor <8luxfshdnlz1@mail.ru>
2021-11-23 13:55:25 +0600

    fixes for staging

--- online_test/serializers.py
+++ online_test/serializers.py
@@ -2,28 +2,29 @@ from rest_framework import serializers
 from .models import OnlineTestQuestion, OnlineTest
 
 
-class OnlineTestQuestionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = OnlineTestQuestion
-        fields = ('question',)
-
-
 class OnlineTestListSerializer(serializers.ModelSerializer):
-    questions = OnlineTestQuestionSerializer(many=True)
+        #  questions = OnlineTestQuestionSerializer(many = True)
 
-    class Meta:
-        model = OnlineTest
-        fields = (
-            'id',
-            'name',
-            'part_num',
-            'version',
-            'duration',
-            'num_questions',
-            'language',
-            'questions')
-        
+	class Meta:
+		model = OnlineTest
+		fields = ('id', 'name', 'intro', 'part_num', 'version', 'duration',
+                        'num_questions', 'language')
 
-class AnswerSerializer(serializers.Serializer):
-    number = serializers.IntegerField()
-    answer = serializers.IntegerField()
+class AnswerSerializer(serializers.ModelSerializer):
+
+	class Meta:
+		model = AnswerTest
+		fields = ('question_number', 'correct_answer')
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+	questions = serializers.SerializerMethodField()
+
+	def get_questions(self, obj):
+		return obj.questions.all().values_list('question', flat=True)
+
+	class Meta:
+		model = OnlineTest
+		fields = ('id', 'name', 'part_num', 'version', 'duration',
+				'num_questions', 'language', 'questions')
+
--- online_test/views.py
+++ online_test/views.py
@@ -1,5 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import OnlineTest
@@ -20,18 +22,39 @@ class OnlineTestViewSet(viewsets.ReadOnlyModelViewSet):
 		'num_questions',
 		'duration']
 
-	@action(detail=True)
-	def check_answers(self, request, pk=None):
-		"""ендпоинт для проверки ответов на вопросы"""
-		answers = OnlineTest.objects.get(pk=pk).answers.all()
-		serializer = AnswerSerializer(data=request.data, many=True)
-		total = 0
-		if serializer.is_valid():
-			for a in serializer.validated_data:
-				current = answers.filter(question_number=a.get('number'))[0]
-				if current.correct_answer == a.get('answer'):
-					total += 1
-		return Response(
-			{'status': 'OK',
-				'number_of_correct_answers': total,
-				'errors': serializer.errors})
+	# @action(detail=True)
+	# def check_answers(self, request, pk=None):
+	# 	"""ендпоинт для проверки ответов на вопросы"""
+	# 	answers = OnlineTest.objects.get(pk=pk).answers.all()
+	# 	serializer = AnswerSerializer(data=request.data, many=True)
+	# 	total = 0
+	# 	if serializer.is_valid():
+	# 		for a in serializer.validated_data:
+	# 			current = answers.filter(question_number=a.get('number'))[0]
+	# 			if current.correct_answer == a.get('answer'):
+	# 				total += 1
+	# 	return Response({'status': 'OK', 'number_of_correct_answers': total, 'errors': serializer.errors})
+
+
+# class OnlineTestListView(ListAPIView):
+# 	queryset = OnlineTest.objects.all()
+# 	serializer_class = OnlineTestListSerializer
+# 	filter_backends = [DjangoFilterBackend]
+# 	filterset_fields = ['language', 'part_num', 'version', 'num_questions', 'duration']
+
+
+class TestQuestionsView(APIView):
+	def get(self, request, pk):
+		online_test = OnlineTest.objects.get(pk=pk)
+		serializer = QuestionSerializer(online_test)
+		return Response(serializer.data)
+
+
+class CheckTestAnswersView(APIView):
+	def get(self, request, pk):
+		online_test = OnlineTest.objects.get(pk=pk)
+		test = OnlineTestListSerializer(online_test)
+		answers = online_test.answers.all()
+		data = {a.question_number: a.correct_answer for a in answers}
+		return Response({'test': test.data, 'answers': data})
+
