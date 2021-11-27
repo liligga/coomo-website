c2fcd9dbac9690f965ef64e1a1d32f1254ae4bf5
Дмитрий Макаров <maggot8950@gmail.com>
2021-11-28 02:08:06 +0600

    Добавлена ссылка на новость в пункте меню

--- menu/serializers.py
+++ menu/serializers.py
@@ -1,11 +1,14 @@
 from rest_framework import serializers
+
+from news.models import News
 from .models import MenuLink, FooterLink
 
 
 class MenuSerializer(serializers.ModelSerializer):
+    news_slug = serializers.SlugField(source='news.slug')
     class Meta:
         model = MenuLink
-        fields = ('id', 'title', 'link', 'icon', 'lang')
+        fields = ('id', 'title', 'link', 'icon', 'lang', 'news_slug')
 
 
 class FooterSerializer(serializers.ModelSerializer):
