d8a5c0038af114a2614aef29641605ed18921602
Дмитрий Макаров <maggot8950@gmail.com>
2021-12-02 01:30:35 +0600

    Тесты для pages

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,54 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+from pages.models import Page
+
+
+class TestPages(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+        self.page1 = Page.objects.create(title='Студентам', article='Информация для студентов')
+        self.page2 = Page.objects.create(title='Абитуриентам', article='Информация для абитуриентов')
+        self.page3 = Page.objects.create(title='Учителям', article='Информация для учителей')
+
+        self.list_url = reverse('pages_list')
+        self.detail_url = reverse('pages_detail', args=(self.page1.slug,))
+
+        return super().setUp()
+
+    def test_pages_list(self):
+        response = self.client.get(self.list_url)
+
+        data = [
+            {
+                'id': self.page1.id,
+                'title': 'Студентам',
+                'slug': 'studentam'
+            },
+            {
+                'id': self.page2.id,
+                'title': 'Абитуриентам',
+                'slug': 'abiturientam'
+            },
+            {
+                'id': self.page3.id,
+                'title': 'Учителям',
+                'slug': 'uchitelyam'
+            },
+        ]
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 3)
+        self.assertEqual(data, response.json())
+
+    def test_pages_detail(self):
+        response = self.client.get(self.detail_url)
+
+        data = {
+            'id': 1,
+            'title': 'Студентам',
+            'slug': 'studentam',
+            'article': 'Информация для студентов'
+        }
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data, response.json())
--- pages/urls.py
+++ pages/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from pages.views import PageListView, PageDetailView
 
 urlpatterns = [
-    path('api/pages/', PageListView.as_view()),
-    path('api/pages/<slug>', PageDetailView.as_view())
+    path('api/pages/', PageListView.as_view(), name='pages_list'),
+    path('api/pages/<slug>', PageDetailView.as_view(), name='pages_detail')
 ]
