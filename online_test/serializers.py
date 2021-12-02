794ca807fa769a635c01a77d72942c90c639413e
Дмитрий Макаров <maggot8950@gmail.com>
2021-12-03 02:38:53 +0600

    Отображение языка тестов

--- online_test/serializers.py
+++ online_test/serializers.py
@@ -4,6 +4,7 @@ from .models import OnlineTestQuestion, OnlineTest, AnswerTest
 
 class OnlineTestListSerializer(serializers.ModelSerializer):
     name = serializers.CharField(source='get_name_display')
+    lang = serializers.CharField(source='get_lang_display')
 
     class Meta:
         model = OnlineTest
