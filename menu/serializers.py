fc7b44abfedab508da100734ed36d28775dac5a1
Дмитрий Макаров <maggot8950@gmail.com>
2021-12-11 02:54:21 +0600

    Изменение урлов картинок в меню и новостях

--- menu/serializers.py
+++ menu/serializers.py
@@ -7,11 +7,17 @@ from .models import MenuLink, FooterLink
 class MenuSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField()
-    icon = serializers.ImageField(use_url=False)
+    icon = serializers.ImageField()
     lang = serializers.CharField()
     page_slug = serializers.SlugField(source='page.slug', allow_null=True)
     position = serializers.CharField()
     link = serializers.CharField()
+
+    def to_representation(self, instance):
+        response = super(MenuSerializer, self).to_representation(instance)
+        if instance.icon:
+            response['icon'] = instance.icon.url
+        return response
 
 
 class FooterSerializer(serializers.ModelSerializer):
--- news/serializers.py
+++ news/serializers.py
@@ -7,11 +7,17 @@ class NewsSerializer(serializers.Serializer):
     title = serializers.CharField()
     slug = serializers.SlugField()
     excerpt = serializers.CharField()
-    cover = serializers.ImageField(use_url=False)
+    cover = serializers.ImageField()
     lang = serializers.CharField()
     created = serializers.DateTimeField()
     updated = serializers.DateTimeField()
     project = serializers.BooleanField()
+
+    def to_representation(self, instance):
+        response = super(NewsSerializer, self).to_representation(instance)
+        if instance.cover:
+            response['cover'] = instance.cover.url
+        return response
 
 
 class NewsDetailSerializer(serializers.ModelSerializer):
