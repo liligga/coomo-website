3f8f8fc9882e7958597f96c80b44d4c36003c9dc
unknown <maggot8950@gmail.com>
2022-03-19 00:51:01 +0600

    Правки онлайн тестов, 404

--- online_test/views.py
+++ online_test/views.py
@@ -1,5 +1,6 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from .models import OnlineTest
 from rest_framework.views import APIView
@@ -15,14 +16,16 @@ class OnlineTestViewSet(viewsets.ReadOnlyModelViewSet):
 
 class TestQuestionsView(APIView):
     def get(self, request, pk):
-        online_test = OnlineTest.objects.get(pk=pk)
+        online_test = get_object_or_404(OnlineTest, pk=pk)
+        # online_test = OnlineTest.objects.get(pk=pk)
         serializer = QuestionSerializer(online_test)
         return Response(serializer.data)
 
 
 class CheckTestAnswersView(APIView):
     def get(self, request, pk):
-        online_test = OnlineTest.objects.get(pk=pk)
+        online_test = get_object_or_404(OnlineTest, pk=pk)
+        # online_test = OnlineTest.objects.get(pk=pk)
         test = OnlineTestListSerializer(online_test)
         answers = online_test.answers.all()
         data = {a.question_number: a.correct_answer for a in answers}
